b60cabed75690500f43f90c238fea755355cabc2
hwanjuson <hwanjuson@gmail.com>
2022-08-06 16:44:13 +0900

    스킬 다 선택했을 시 인게임 브금으로 전환

--- Parkjein/Assets/_Scripts/HanSocket/Handlers/InGame/SkillAllSelectedHandler.cs
+++ Parkjein/Assets/_Scripts/HanSocket/Handlers/InGame/SkillAllSelectedHandler.cs
@@ -1,3 +1,4 @@
+using Managers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ namespace HanSocket.Handlers.InGame
 
         protected override void OnFlag()
         {
+            SoundManager.Instance.PlayIngameBgm();
             Debug.Log("와 스킬 다 선택했데요");
         }
     }
